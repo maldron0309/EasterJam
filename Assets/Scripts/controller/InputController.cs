06bff4d05e72481e9e9d396e7f9d285da27cba8d
= <26445308+GingaaBread@users.noreply.github.com>
2024-03-16 23:20:02 +0100

    Notification

---
+++ Assets/Scripts/controller/InputController.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+
+namespace controller
+{
+    public class InputController : MonoBehaviour
+    {
+        /// <summary>
+        ///     Provides access to the input master, allowing scripts to subscribe to input events
+        /// </summary>
+        public InputMaster InputMaster { private set; get; }
+
+        /// <summary>
+        ///     The singleton instance
+        /// </summary>
+        public static InputController Instance { get; private set; }
+
+        /// <summary>
+        ///     Creates the singleton instance and manages the DDOL object
+        /// </summary>
+        private void Awake()
+        {
+            if (Instance != null) return;
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+            InputMaster = new();
+            InputMaster.Enable();
+        }
+
+        private void OnEnable()
+        {
+            InputMaster.Enable();
+        }
+
+        private void OnDisable()
+        {
+            InputMaster.Disable();
+        }
+    }
+}
--- Assets/Scripts/mono/ui/MessageCanvas.cs
+++ Assets/Scripts/mono/ui/MessageCanvas.cs
@@ -1,4 +1,5 @@
-﻿using TMPro;
+﻿using System.Collections;
+using TMPro;
 using UnityEngine;
 using UnityEngine.Assertions;
 using UnityEngine.Localization;
@@ -11,9 +12,10 @@ namespace mono.ui
         public const string NOT_ENOUGH_CHECKPOINT_MANA_KEY_NAME = "not-enough-checkpoint-mana";
 
         [SerializeField] private LocalizedStringTable _hudMessagesTable;
-        [SerializeField] private GameObject _panel;
+        [SerializeField] private RectTransform _panel;
         [SerializeField] private GameObject _container;
         [SerializeField] private TMP_Text _messageText;
+        [SerializeField] private TextMesh _messageTextMesh;
 
         public static MessageCanvas Instance { get; private set; }
 
@@ -25,12 +27,30 @@ namespace mono.ui
 
         public void ShowMessage(string keyName)
         {
-            _panel.transform.localScale = new Vector2(0.5f, 0.5f);
+            _container.SetActive(true);
+
+            _panel.localScale = new Vector2(0.5f, 0.5f);
             LeanTween
                 .scale(_panel, Vector3.one, 1.5f)
                 .setEase(LeanTweenType.easeOutElastic);
+            LeanTween.alpha(_panel, 1f, 0.3f);
 
             _messageText.text = _hudMessagesTable.GetTable().GetEntry(keyName).Value;
+
+            StartCoroutine(FadeOutAfterTime());
+        }
+
+        private IEnumerator FadeOutAfterTime()
+        {
+            yield return new WaitForSeconds(4);
+
+            _messageText.alpha = 0f;
+            LeanTween.alpha(_panel, 0f, 0.6f);
+
+            yield return new WaitForSeconds(1f);
+
+            _messageText.alpha = 1f;
+            _container.SetActive(false);
         }
     }
 }
