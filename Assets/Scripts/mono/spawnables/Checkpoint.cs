c9113f0c600c662a001c62711ad157253bd71099
= <26445308+GingaaBread@users.noreply.github.com>
2024-03-21 10:27:03 +0100

    Checkpoint Spawn Animation

--- Assets/Scripts/mono/spawnables/Checkpoint.cs
+++ Assets/Scripts/mono/spawnables/Checkpoint.cs
@@ -4,6 +4,13 @@ namespace mono.spawnables
 {
     public class Checkpoint : MonoBehaviour
     {
-        
+        private Animator _animator;
+
+        private void Awake()
+        {
+            _animator = GetComponent<Animator>();
+        }
+
+        public void PlaySpawnAnimation() => _animator.Play("Checkpoint Spawn");
     }
 }
--- Assets/Scripts/mono/spawner/CheckpointSpawner.cs
+++ Assets/Scripts/mono/spawner/CheckpointSpawner.cs
@@ -1,14 +1,17 @@
-﻿using mono.spawnables;
+﻿using System.Collections;
+using mono.spawnables;
+using mono.ui;
 using UnityEngine;
 using UnityEngine.Events;
 
 namespace mono.spawner
 {
+    [RequireComponent(typeof(Animator))]
     public class CheckpointSpawner : MonoBehaviour
     {
+        private const float OFFSET = 2f;
         [SerializeField] private Checkpoint _checkpointPrefab;
         [SerializeField] private UnityEvent _onCheckpointSpawned;
-
 
         private Checkpoint _checkpoint;
 
@@ -35,9 +38,19 @@ namespace mono.spawner
             if (_checkpoint == null) _checkpoint = Instantiate(_checkpointPrefab);
 
             // Move to the player position
-            _checkpoint.transform.position = transform.position;
+            var transformPosition = transform.position;
+            _checkpoint.transform.position = new Vector2(transformPosition.x, transformPosition.y + OFFSET);
+
+            _checkpoint.PlaySpawnAnimation();
+            StartCoroutine(ShowMessageAfterDelay());
 
             _onCheckpointSpawned.Invoke();
+        }
+
+        private static IEnumerator ShowMessageAfterDelay()
+        {
+            yield return new WaitForSeconds(1f);
+            MessageCanvas.Instance.ShowMessage(MessageCanvas.CHECKPOINT_SPAWNED_KEY_NAME, true);
         }
     }
 }
